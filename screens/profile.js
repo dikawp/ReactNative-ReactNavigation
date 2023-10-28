c6fcd6a40780e05a66b25e08229441d2d52ed06e
dikawp <ramadika380@gmail.com>
2023-10-28 19:42:30 +0700

    first commit

---
+++ screens/profile.js
@@ -0,0 +1,18 @@
+import { Heading, Center } from "native-base";
+import { Header } from "../components";
+
+const Profile = () => {
+  return (
+    <>
+      <Header title={"Profile"} />
+      <Center flex={1}>
+        <Heading>Profile</Heading>
+      </Center>
+    </>
+  );
+};
+
+export default Profile;
+
+
+
